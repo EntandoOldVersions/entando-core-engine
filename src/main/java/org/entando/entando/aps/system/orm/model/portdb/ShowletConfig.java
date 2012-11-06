3f64ba398d11a4906ea372d4c66fc6cef8c9a72e
eugeniosant <eugenio.santoboni@gmail.com>
2012-11-06 17:52:09 +0100

    License upgrade

--- src/main/java/org/entando/entando/aps/system/orm/model/portdb/ShowletConfig.java
+++ src/main/java/org/entando/entando/aps/system/orm/model/portdb/ShowletConfig.java
@@ -1,7 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+*
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
+*
+* This file is part of Entando software.
+* Entando is a free software; 
+* you can redistribute it and/or modify it
+* under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
+* 
+* See the file License for the specific language governing permissions   
+* and limitations under the License
+* 
+* 
+* 
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
+*
+*/
 package org.entando.entando.aps.system.orm.model.portdb;
 
 import com.j256.ormlite.field.DataType;
