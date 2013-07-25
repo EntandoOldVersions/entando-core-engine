1fcfd1ad2924857222709a5925e3dd53fb41c1c8
eugeniosant <eugenio.santoboni@gmail.com>
2013-07-25 12:39:12 +0200

    Fix imports

--- src/main/java/com/agiletec/plugins/jacms/aps/system/services/content/showlet/ContentListHelper.java
+++ src/main/java/com/agiletec/plugins/jacms/aps/system/services/content/showlet/ContentListHelper.java
@@ -46,6 +46,7 @@ import com.agiletec.plugins.jacms.aps.system.services.content.showlet.util.Filte
 
 import org.entando.entando.aps.system.services.cache.CacheableInfo;
 import org.entando.entando.aps.system.services.cache.ICacheInfoManager;
+import org.entando.entando.aps.system.services.searchengine.IEntitySearchEngineManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
