f32133bdba60ec42ca969077efbcc946c4202d94
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2013-09-30 23:58:17 +0200

    Api DOM refactored

--- src/main/java/org/entando/entando/aps/system/services/api/ApiResourcesDefDOM.java
+++ src/main/java/org/entando/entando/aps/system/services/api/ApiResourcesDefDOM.java
@@ -206,10 +206,10 @@ public class ApiResourcesDefDOM {
     public static final String PARAMETER_TYPE_ATTRIBUTE_NAME = "type";
     public static final String PARAMETER_OVERRIDE_ATTRIBUTE_NAME = "override";
     public static final String PARAMETER_DESCRIPTION_ELEMENT_NAME = "description";
-    public static final String RELATED_WIDGET_ELEMENT_NAME = "relatedShowlet";
+    public static final String RELATED_WIDGET_ELEMENT_NAME = "relatedWidget";
     public static final String RELATED_WIDGET_CODE_ATTRIBUTE_NAME = "code";
     public static final String RELATED_WIDGET_MAP_PARAMETER_ELEMENT_NAME = "mapParameter";
-    public static final String RELATED_WIDGET_MAP_PARAMETER_WIDGET_ATTRIBUTE_NAME = "showlet";
+    public static final String RELATED_WIDGET_MAP_PARAMETER_WIDGET_ATTRIBUTE_NAME = "widget";
     public static final String RELATED_WIDGET_MAP_PARAMETER_METHOD_ATTRIBUTE_NAME = "method";
         
 	/**
