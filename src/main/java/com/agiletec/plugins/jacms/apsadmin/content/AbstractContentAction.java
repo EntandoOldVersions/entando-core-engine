abbe191a0a7444cdcc02c8d16d489e6d88d7cbac
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2013-09-30 23:58:33 +0200

    Java refactoring

--- src/main/java/com/agiletec/plugins/jacms/apsadmin/content/AbstractContentAction.java
+++ src/main/java/com/agiletec/plugins/jacms/apsadmin/content/AbstractContentAction.java
@@ -2,9 +2,9 @@
 *
 * Copyright 2013 Entando S.r.l. (http://www.entando.com) All rights reserved.
 *
-* This file is part of Entando Enterprise Edition software.
+* This file is part of Entando software.
 * You can redistribute it and/or modify it
-* under the terms of the Entando's EULA
+* under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
 * 
 * See the file License for the specific language governing permissions   
 * and limitations under the License
@@ -109,12 +109,6 @@ public abstract class AbstractContentAction extends BaseAction {
 	 * @return La lista di stati di contenuto definiti nel sistema.
 	 */
 	public List<SelectItem> getAvalaibleStatus() {
-		
-		System.out.println(">>>>> getAvalaibleStatus INVOCATO <<<<<");
-		System.out.println(">>>>> getAvalaibleStatus INVOCATO <<<<<");
-		System.out.println(">>>>> getAvalaibleStatus INVOCATO <<<<<");
-		System.out.println(">>>>> getAvalaibleStatus INVOCATO <<<<<");
-		
 		String[] status = Content.AVAILABLE_STATUS;
 		List<SelectItem> items = new ArrayList<SelectItem>(status.length);
 		for (int i = 0; i < status.length; i++) {
