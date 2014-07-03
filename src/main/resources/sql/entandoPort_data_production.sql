22b999a56a7179a23c5614db81c4d679f64818ff
eugeniosant <eugenio.santoboni@gmail.com>
2014-07-03 11:17:07 +0200

    Add widget login fragment

--- src/main/resources/sql/entandoPort_data_production.sql
+++ src/main/resources/sql/entandoPort_data_production.sql
@@ -1796,6 +1796,79 @@ INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, lock
 	<a class="btn btn-primary" href="<@wp.action path="/ExtStr2/do/Front/Api/Resource/list.action" />"><span class="icon-arrow-left icon-white"></span>&#32;<@wp.i18n key="ENTANDO_API_GOTO_LIST" /></a>
 </p>
 </div>', 1);
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('login_form', 'login_form', NULL, NULL, '<#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
+<#assign wp=JspTaglibs["/aps-core"]>
+
+<h1><@wp.i18n key="RESERVED_AREA" /></h1>
+
+<#if (Session.currentUser.username != "guest") >
+	
+	<p><@wp.i18n key="WELCOME" />, <em>${Session.currentUser}</em>!</p>
+	<#if (Session.currentUser.entandoUser) >
+	<table class="table table-condensed">
+		<tr>
+			<th><@wp.i18n key="USER_DATE_CREATION" /></th>
+			<th><@wp.i18n key="USER_DATE_ACCESS_LAST" /></th>
+			<th><@wp.i18n key="USER_DATE_PASSWORD_CHANGE_LAST" /></th>
+		</tr>
+		<tr>
+			<td>${Session.currentUser.creationDate}</td>
+			<td>${Session.currentUser.lastAccess}</td>
+			<td>${Session.currentUser.lastPasswordChange}</td>
+		</tr>
+	</table>
+		<#if (!Session.currentUser.credentialsNotExpired) >
+		<div class="alert alert-block">
+			<p><@wp.i18n key="USER_STATUS_EXPIRED_PASSWORD" />: <a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/editPassword.action"><@wp.i18n key="USER_STATUS_EXPIRED_PASSWORD_CHANGE" /></a></p>
+		</div>
+		</#if>
+	</#if>
+	<@wp.ifauthorized permission="enterBackend">
+	<#-- <h2><@wp.i18n key="ADMINISTRATION" />:</h2> -->
+	<div class="btn-group">
+		<a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/main.action?request_locale=<@wp.info key="currentLang" />" class="btn"><@wp.i18n key="ADMINISTRATION" /></a>
+	</div>
+	</@wp.ifauthorized>
+	<p class="pull-right"><a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/logout.action" class="btn"><@wp.i18n key="LOGOUT" /></a></p>
+	
+	<@wp.pageWithWidget widgetTypeCode="userprofile_editCurrentUser" var="userprofileEditingPageVar" listResult=false />
+	<#if (userprofileEditingPageVar??) >
+	<p><a href="<@wp.url page="${userprofileEditingPageVar.code}" />" ><@wp.i18n key="userprofile_CONFIGURATION" /></a></p>
+	</#if>
+	
+<#else>
+	<#if (accountExpired?? && accountExpired == true) >
+	<div class="alert alert-block alert-error">
+		<p><@wp.i18n key="USER_STATUS_EXPIRED" /></p>
+	</div>
+	</#if>
+	<#if (wrongAccountCredential?? && wrongAccountCredential == true) >
+	<div class="alert alert-block alert-error">
+		<p><@wp.i18n key="USER_STATUS_CREDENTIALS_INVALID" /></p>
+	</div>
+	</#if>
+	
+	<form action="<@wp.url/>" method="post" class="form-horizontal margin-medium-top">
+		<#if (RequestParameters.returnUrl??) >
+		<input type="hidden" name="returnUrl" value="${RequestParameters.returnUrl}" />
+		</#if>
+	<div class="control-group">
+			<label for="username" class="control-label"><@wp.i18n key="USERNAME" /></label>
+			<div class="controls">
+				<input id="username" type="text" name="username" class="input-xlarge" />
+			</div>
+		</div>
+		<div class="control-group">
+			<label for="password" class="control-label"><@wp.i18n key="PASSWORD" /></label>
+			<div class="controls">
+				<input id="password" type="password" name="password" class="input-xlarge" />
+		</div>
+		<div class="form-actions">
+			<input type="submit" value="<@wp.i18n key="SIGNIN" />" class="btn btn-primary" />
+		</div>
+	</form>
+	
+</#if>', 1);
 
 
 
